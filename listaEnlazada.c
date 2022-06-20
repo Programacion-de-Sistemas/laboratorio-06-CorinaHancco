1af65b442e3552355571655ec2087bfdde32cf60
Corina <chanccov@unsa.edu.pe>
2022-06-19 21:28:33 -0500

    Prueba de agregar

--- listaEnlazada.c
+++ listaEnlazada.c
@@ -25,26 +25,27 @@ int main(){
     node *list=NULL;
 
     //agregamos un elemento a la lista
-    node *n = malloc(sizeof(node)); //asignando memoria para node
-    if(n==NULL){
-        return 1;
-    }
-    n->number=45;
-    n->next=NULL;
+   list = addNode(list,4) 
+   // node *n = malloc(sizeof(node)); //asignando memoria para node
+   // if(n==NULL){
+     //  return 1;
+   // }
+   // n->number=45;
+   // n->next=NULL;
 
-    node *n_2 = malloc(sizeof(node)); //asignando memoria para node
-    if(n_2==NULL){
-        return 1;
-    }
-    n_2->number=15;
-    n_2->next=NULL;
+   // node *n_2 = malloc(sizeof(node)); //asignando memoria para node
+   // if(n_2==NULL){
+     //   return 1;
+   // }
+   // n_2->number=15;
+   // n_2->next=NULL;
 
     //actualizar el punto next del primer elemento
-    n->next=n_2;
+   // n->next=n_2;
 
 
     //actualizamos la lista
-    list=n;
+   // list=n;
 
     // imprimir los elementos de la lista
     for (node *tmp = list;tmp!=NULL;tmp=tmp->next)
